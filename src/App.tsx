5ff2d12f06ab626e4da1154145d57f3b3dbfc790
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-02-14 15:08:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import Home from './paginas/home/Home'
+
+function App() {
+  return (
+  <Home />)
+}
+
+export default App
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home() {
+    return (
+      <>
+      <h1 className="titulo">kaizoku oni ore wa naru</h1>
+      <img className="img" src="https://tm.ibxk.com.br/2015/09/09/09165955100389.jpg?ims=1200x675" alt="Imagem do futuro rei dos piratas" />
+      </>
+    )
+  }
+  
+  export default Home;
